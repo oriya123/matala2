7da2a942cf697141789cc7b0d3e1a0555a147d35
oriya123 <oriya0531@gmail.com>
2021-03-26 11:54:39 +0200

    first

--- text.py
+++ text.py
@@ -1,37 +1,23 @@
-def revword (word:str)-> str:
+def revword(word:str) -> str:
     sizeword=len(word)
-    result=''
-    for i in range (sizeword):
-        result=result +word[sizeword-i-1]
+    result=""
+    for i in range(sizeword):
+        result=result+word[sizeword-i-1]
     return result.lower()
 
-def countword()->int:
+def countword()-> int:
+    counter=1
     fhand=open('text.txt')
     word=fhand.readline()
-    counter=1
-    for line in fhand:
-        words=line.split()
-        numberofwordsinline=len(words)
-        for i in range(numberofwordsinline):
+    word=word.lower()
+    for line in fhand:  
+        words1=line.split() 
+        numberofwords=len(words1)
+        for i in range(numberofwords):        
             a=line.split()
             b=revword(a[i])
-            if (word.strip()==b.strip()):
-                counter=counter+1    
+            if(word.strip()==b.strip()):
+                counter=counter+1      
     return counter
-
-
-
-
-
-        
-    
-   
-
-
-
     
 
-
-
-
-    
